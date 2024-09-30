c5bdd539dc25aea05e24830da6007114435aedc4
Shuhiro Mayorga <shuhiro123@hotmail.com>
2024-09-30 00:37:24 -0500

    Observabel tipo subject y behaviorSubject

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,5 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import { BehaviorSubject, Observable, Observer, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,13 @@ export class MultimediaService {
 
   callback:EventEmitter<any> = new EventEmitter<any>;
 
-  myObservable1$:Observable<any> = new Observable(
+  //Subject
+  //myObservable1$: Subject<any> = new Subject();
+  
+  myObservable1$: BehaviorSubject<any> = new BehaviorSubject('***');
+
+  //Observable y Observer
+  /* myObservable1$:Observable<any> = new Observable(
     (observer:Observer<any>)=>{
       observer.next('****')
       
@@ -18,7 +24,19 @@ export class MultimediaService {
 
       setTimeout(()=>{observer.error('.-.-.-.-.-')},3500)
     }
-  );
+  ); */
 
-  constructor() { }
+  constructor() {
+
+    //Para => myObservable1$: Subject<any> = new Subject();
+    /* setTimeout(()=>{
+    this.myObservable1$.next('*1*')
+    },1000); */
+    //this.myObservable1$.next('***')
+
+
+    setTimeout(()=>{
+    this.myObservable1$.next('*1*')
+    },1000);
+  }
 }
